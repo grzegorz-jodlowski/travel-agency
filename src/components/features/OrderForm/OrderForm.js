173c7c533b63e0be97cfa0ebff0161bef879d290
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-18 17:56:49 +0100

    Map pricing.json to OrderOption components

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
 
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = ({ tripCost, options }) => (
   <Row>
+    {pricing.map(option =>
+      <Col md={4} key={option.id} >
+        <OrderOption {...option} />
+      </Col>
+    )}
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>

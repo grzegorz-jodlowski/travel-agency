615235b873b4089d66766abc731f3ce5d9fc4468
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-18 18:04:30 +0100

    Add prop types and title to OrderOption component

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './OrderOption.scss';
 
-const OrderOption = () => (
+const OrderOption = ({ name }) => (
   <div className={styles.component}>
-    <h3 className={styles.title}>Car rental</h3>
+    <h3 className={styles.title}>{name}</h3>
   </div>
 );
 
+OrderOption.propTypes = {
+  name: PropTypes.string,
+};
 
 export default OrderOption;

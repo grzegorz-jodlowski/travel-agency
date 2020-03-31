c1ac04ceb82a4e35c5942e0382d005244bc3e9a2
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-31 16:19:28 +0200

    Add OrderOption comp test for render without props

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -8,4 +8,11 @@ describe('Component OrderOption', () => {
     expect(component).toBeTruthy();
     console.log(component.debug());
   });
+
+  it('should return empty object if called without required props', () => {
+    const component = shallow(<OrderOption name='hello' />);
+    expect(component).toEqual({});
+    console.log(component.debug());
+  });
+
 });

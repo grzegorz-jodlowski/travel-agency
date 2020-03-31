84ce1b9eeb602123d66d770f7a74d41a4758c87d
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-31 13:31:59 +0200

    Add TripSummary test for src and alt rendering

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -6,6 +6,13 @@ describe('Component TripSummary', () => {
   it('should generate link to correct url', () => {
     const component = shallow(<TripSummary id='abc' tags={[]} />);
     expect(component.find('Link').prop('to')).toBe('/trip/abc');
+    console.log(component.debug());
   });
 
+  it('should render correct src and alt for img', () => {
+    const component = shallow(<TripSummary image='image.png' name='description' tags={[]} />);
+    expect(component.find('img').prop('src')).toBe('image.png');
+    expect(component.find('img').prop('alt')).toBe('description');
+    console.log(component.debug());
+  });
 });

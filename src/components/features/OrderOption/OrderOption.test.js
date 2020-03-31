94d77a092328ba52be5e96e1f1b3913ce3d1cf44
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-31 16:31:40 +0200

    Add OrderOption comp test for render correct title

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -3,16 +3,23 @@ import { shallow } from 'enzyme';
 import OrderOption from './OrderOption';
 
 describe('Component OrderOption', () => {
+  const expectedName = 'hello';
+  const expectedType = 'icons';
+
   it('should render without crashing', () => {
-    const component = shallow(<OrderOption name='hello' type='icon' />);
+    const component = shallow(<OrderOption name={expectedName} type={expectedType} />);
     expect(component).toBeTruthy();
     console.log(component.debug());
   });
 
   it('should return empty object if called without required props', () => {
-    const component = shallow(<OrderOption name='hello' />);
+    const component = shallow(<OrderOption name={expectedName} />);
     expect(component).toEqual({});
     console.log(component.debug());
   });
 
+  it('should render correct title', () => {
+    const component = shallow(<OrderOption name={expectedName} type={expectedType} />);
+    expect(component.find('.title').text()).toBe(expectedName);
+  });
 });

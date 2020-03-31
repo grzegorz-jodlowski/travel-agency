e1d67f53998a51481fd582bc81c49a0564919c1d
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-31 14:08:29 +0200

    Add TripSummary comp test for required props

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -4,23 +4,31 @@ import TripSummary from './TripSummary';
 
 describe('Component TripSummary', () => {
   it('should generate link to correct url', () => {
-    const component = shallow(<TripSummary id='abc' tags={[]} />);
+    const component = shallow(<TripSummary id='abc' name='Marvelous travel' cost='$139' days={14} image='image.png' tags={[]} />);
     expect(component.find('Link').prop('to')).toBe('/trip/abc');
     console.log(component.debug());
   });
 
   it('should render correct src and alt for img', () => {
-    const component = shallow(<TripSummary image='image.png' name='description' tags={[]} />);
+    const component = shallow(<TripSummary id='abc' name='Marvelous travel' cost='$139' days={14} image='image.png' tags={[]} />);
     expect(component.find('img').prop('src')).toBe('image.png');
-    expect(component.find('img').prop('alt')).toBe('description');
+    expect(component.find('img').prop('alt')).toBe('Marvelous travel');
     console.log(component.debug());
   });
 
   it('should render correct name, cost and days props', () => {
-    const component = shallow(<TripSummary name='Marvelous travel' cost='$139' days={14} tags={[]} />);
+    const component = shallow(<TripSummary id='abc' name='Marvelous travel' cost='$139' days={14} image='image.png' tags={[]} />);
     expect(component.find('.title').text()).toBe('Marvelous travel');
     expect(component.find('.days').text()).toBe('14 days');
     expect(component.find('.cost').text()).toBe('from $139');
     console.log(component.debug());
+  });
+
+  it('should throw an error without required props', () => {
+    expect(() => <TripSummary id='abc' name='Marvelous travel' cost='$139' days={14} />).toThrow();
+    expect(() => <TripSummary id='abc' name='Marvelous travel' cost='$139' />).toThrow();
+    expect(() => <TripSummary id='abc' name='Marvelous travel' />).toThrow();
+    expect(() => <TripSummary id='abc' />).toThrow();
+    expect(() => <TripSummary />).toThrow();
   });
 });

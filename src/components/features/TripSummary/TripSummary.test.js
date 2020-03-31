fe2305683f17874431c2ed073a5f1b172a26031a
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-03-31 15:04:17 +0200

    Add TripSummary comp test for tags

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -6,14 +6,12 @@ describe('Component TripSummary', () => {
   it('should generate link to correct url', () => {
     const component = shallow(<TripSummary id='abc' name='Marvelous travel' cost='$139' days={14} image='image.png' tags={[]} />);
     expect(component.find('Link').prop('to')).toBe('/trip/abc');
-    console.log(component.debug());
   });
 
   it('should render correct src and alt for img', () => {
     const component = shallow(<TripSummary id='abc' name='Marvelous travel' cost='$139' days={14} image='image.png' tags={[]} />);
     expect(component.find('img').prop('src')).toBe('image.png');
     expect(component.find('img').prop('alt')).toBe('Marvelous travel');
-    console.log(component.debug());
   });
 
   it('should render correct name, cost and days props', () => {
@@ -21,7 +19,6 @@ describe('Component TripSummary', () => {
     expect(component.find('.title').text()).toBe('Marvelous travel');
     expect(component.find('.details > span').at(0).text()).toBe('14 days');
     expect(component.find('.details > span').at(1).text()).toBe('from $139');
-    console.log(component.debug());
   });
 
   it('should throw an error without required props', () => {
@@ -30,5 +27,17 @@ describe('Component TripSummary', () => {
     expect(() => <TripSummary id='abc' name='Marvelous travel' />).toThrow();
     expect(() => <TripSummary id='abc' />).toThrow();
     expect(() => <TripSummary />).toThrow();
+  });
+
+  it('should render correct array with tags', () => {
+    const component = shallow(<TripSummary id='abc' name='Marvelous travel' cost='$139' days={14} image='image.png' tags={['tag1', 'tag2', 'tag3']} />);
+    expect(component.find('.tag').at(0).text()).toBe('tag1');
+    expect(component.find('.tag').at(1).text()).toBe('tag2');
+    expect(component.find('.tag').at(2).text()).toBe('tag3');
+  });
+
+  it('shouldn\'t render <div> with class tags when tags array is empty', () => {
+    const component = shallow(<TripSummary id='abc' name='Marvelous travel' cost='$139' days={14} image='image.png' tags={[]} />);
+    expect(component.exists('.tags')).toBeFalsy();
   });
 });

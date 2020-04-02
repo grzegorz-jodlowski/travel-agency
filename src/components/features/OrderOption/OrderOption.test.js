537e9d8edd8b4579b10d0886670370c9bcd64214
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-02 17:30:28 +0200

    Add tests for dropdown subcomponent of OrderOption

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -85,13 +85,24 @@ for (let type in optionTypes) {
     it(`renders ${optionTypes[type]}`, () => {
       expect(subcomponent).toBeTruthy();
       expect(subcomponent.length).toBe(1);
-      console.log(subcomponent.debug());
     });
 
     /* type-specific tests */
     switch (type) {
       case 'dropdown': {
-        /* tests for dropdown */
+        it('contains select and options', () => {
+          const select = renderedSubcomponent.find('select');
+          expect(select.length).toBe(1);
+
+          const emptyOption = select.find('option[value=""]').length;
+          expect(emptyOption).toBe(1);
+
+          const options = select.find('option').not('[value=""]');
+          expect(options.length).toBe(mockProps.values.length);
+          expect(options.at(0).prop('value')).toBe(mockProps.values[0].id);
+          expect(options.at(1).prop('value')).toBe(mockProps.values[1].id);
+          console.log(subcomponent.debug());
+        });
         break;
       }
     }

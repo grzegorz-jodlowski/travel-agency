9019fb7ac9d15a749f433b87362a1983c438c8f2
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-04 19:08:24 +0200

    Refactor OrderOption test - remove console.logs

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -11,13 +11,11 @@ describe('Component OrderOption', () => {
   it('should render without crashing', () => {
     const component = shallow(<OrderOption name={expectedName} type={expectedType} />);
     expect(component).toBeTruthy();
-    console.log(component.debug());
   });
 
   it('should return empty object if called without required props', () => {
     const component = shallow(<OrderOption name={expectedName} />);
     expect(component).toEqual({});
-    console.log(component.debug());
   });
 
   it('should render correct title', () => {

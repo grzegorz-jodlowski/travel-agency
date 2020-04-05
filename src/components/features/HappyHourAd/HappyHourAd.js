7f67db3378951391007fc4ada48d95be7a1cff22
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-05 16:56:11 +0200

    Add props title for HappyHourAd component

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from './HappyHourAd.scss';
+import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
   render() {
     return (
       <div>
-        <h3 className={styles.title}>Happy Hour</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
         <div className={styles.promoDescription}>Countdown</div>
       </div>
     );
   }
 }
+
+HappyHourAd.propTypes = {
+  title: PropTypes.string,
+};
 
 export default HappyHourAd;

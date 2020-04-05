635fe2c515adeb2c40df649baf7225da0c3964c7
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-05 16:53:28 +0200

    Add test for ttitle of HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+  title: '.title',
+  promoDescription: '.promoDescription',
+};
+
+const mockProps = {
+  title: 'Happy Hours',
+  promoDescription: `It's your time! Take advantage of Happy Hour! All offers 20% off!`,
+};
+
 describe('Component HappyHourAd', () => {
-  const select = {
-    title: '.title',
-    promoDescription: '.promoDescription',
-  };
 
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
@@ -17,5 +23,10 @@ describe('Component HappyHourAd', () => {
     const component = shallow(<HappyHourAd />);
     expect(component.exists(select.title)).toEqual(true);
     expect(component.exists(select.promoDescription)).toEqual(true);
+  });
+
+  it('should render title from props', () => {
+    const component = shallow(<HappyHourAd {...mockProps} />);
+    expect(component.find(select.title).text()).toBe(mockProps.title);
   });
 });

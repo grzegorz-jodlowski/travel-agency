efcbcd7a9c3d9ff38aca6099e1b82bbb4b26795f
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-05 16:41:28 +0200

    Add test for title and countdown in HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -3,8 +3,19 @@ import { shallow } from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
 describe('Component HappyHourAd', () => {
+  const select = {
+    title: '.title',
+    promoDescription: '.promoDescription',
+  };
+
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
+  });
+
+  it('should render title and countdown div', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
   });
 });

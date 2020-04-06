08294670ae760af6afa086f8fb100d1e9b15df04
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-06 19:16:52 +0200

    Add counter for seconds to next noon

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -3,11 +3,22 @@ import styles from './HappyHourAd.scss';
 import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
+
+  getCountdownTime() {
+    const currentTime = new Date();
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1);
+    }
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000);
+  }
   render() {
     return (
-      <div>
+      <div className={styles.component}>
         <h3 className={styles.title}>{this.props.title}</h3>
-        <div className={styles.promoDescription}>Countdown</div>
+        <div className={styles.promoDescription}>{this.getCountdownTime()}</div>
       </div>
     );
   }
@@ -15,6 +26,7 @@ class HappyHourAd extends React.Component {
 
 HappyHourAd.propTypes = {
   title: PropTypes.string,
+  promoDescription: PropTypes.string,
 };
 
 export default HappyHourAd;

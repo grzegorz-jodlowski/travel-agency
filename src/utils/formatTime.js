f6f79da7d2653350055606612cea0e53979e2d01
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-07 13:34:37 +0200

    Add correct time formatting in formatTime function

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,11 @@
 export const formatTime = (seconds) => {
-  if (seconds === undefined || typeof seconds !== 'number') {
+  if (seconds === undefined || typeof seconds !== 'number' || seconds < 0) {
     return null;
   } else {
-    return 'formatted time';
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds / 60) % 60);
+    const s = Math.floor(seconds % 60);
+
+    return `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`;
   }
 };

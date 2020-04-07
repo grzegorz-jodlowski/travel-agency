aa96052bd20cd7453c71b3758293bf6be7412e53
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-07 13:42:38 +0200

    Refactor formatTime function

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -2,10 +2,6 @@ export const formatTime = (seconds) => {
   if (seconds === undefined || typeof seconds !== 'number' || seconds < 0) {
     return null;
   } else {
-    const h = Math.floor(seconds / 3600);
-    const m = Math.floor((seconds / 60) % 60);
-    const s = Math.floor(seconds % 60);
-
-    return `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`;
+    return `${String(Math.floor(seconds / 3600)).padStart(2, '0')}:${String(Math.floor((seconds / 60) % 60)).padStart(2, '0')}:${String(Math.floor(seconds % 60)).padStart(2, '0')}`;
   }
 };
